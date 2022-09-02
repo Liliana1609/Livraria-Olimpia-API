e897355408ad52f55debfe95d99ed9bb6c9e40b1
Liliana1609 <lily.webdev22@gmail.com>
2022-09-02 16:50:04 -0400

    Add de variável de ambiente na porta

--- src/app.js
+++ src/app.js
@@ -22,7 +22,7 @@ routerEstoque(app)
 routerFuncionario(app)
 routerLivros(app)
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.listen(port, ()=> {
     console.log(`http://localhost:${port}/`)

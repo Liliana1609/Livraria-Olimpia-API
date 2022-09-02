fbe84a1ac310872018b821b138e9506089e70135
nathuchoa <97313973+nathuchoa@users.noreply.github.com>
2022-09-02 21:25:09 +0100

    Add cors

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 import routerCliente from "./router/routerCliente.js";
 import pagamentos from "./router/routerPagamentos.js";
 import fornecedores from "./router/fornecedoresRouter.js"
@@ -6,7 +7,10 @@ import routerEstoque from "./router/routerEstoque.js";
 import { routerFuncionario } from "./router/routerFuncionario.js";
 import routerLivros from "./router/routerLivros.js";
 
+
 const app = express()
+
+app.use(cors());
 
 app.use(express.json())
 
